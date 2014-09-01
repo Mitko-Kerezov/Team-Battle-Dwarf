d5652adebcf7b4d35435afe77c46909fe84b45f7
Mitko-Kerezov <d.kerezov@gmail.com>
2014-09-01 16:33:45 +0300

    Initial commit, database first approach

---
+++ SummerOlympiadsApplication/SummerOlympiadsConsoleClient/EntryPoint.cs
@@ -0,0 +1,18 @@
+﻿namespace SummerOlympiads.ConsoleClient
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+     using System.Data.Entity;
+
+    using Model;
+    class EntryPoint
+    {
+        static void Main()
+        {
+            Console.WriteLine("#TODO: Add records");
+        }
+    }
+}
