73542ffd89a1a189ca07d4d39e41ef840d027dd0
Zhiko Mitov <mitov90@gmail.com>
2014-09-02 12:38:11 +0300

    Added excel reader row by row

--- SummerOlympiadsApplication/SummerOlympiadsConsoleClient/EntryPoint.cs
+++ SummerOlympiadsApplication/SummerOlympiadsConsoleClient/EntryPoint.cs
@@ -2,15 +2,25 @@
 {
     using System;
 
+    using SummerOlympiads.Data.Excel;
     using SummerOlympiads.Model;
+    using SummerOlympiads.Utils;
 
     internal class EntryPoint
     {
         private static void Main()
         {
-            // TODO: add records in db
             using (var db = new OlympiadsEntities())
             {
+                var files = ZipHandler.ExtractDefaultFile();
+                foreach (var file in files)
+                {
+                    var importer = new ExcelReader(file);
+                    foreach (var row in importer)
+                    {
+                        Console.WriteLine(row);
+                    }
+                }
             }
         }
     }
